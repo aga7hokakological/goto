b78982d527b35b1e8215c847ece9c381c287eb12
aga7hokakological <iamsaurabhmandavkar@gmail.com>
2020-08-26 00:19:47 +0530

    cmd args

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use std::env;
 use std::process::{Command, Stdio};
 
 
 fn main() {
-	Command::new("ls")
-        .stdin(Stdio::null())
+	let args: Vec<String> = env::args().collect();
+	println!("{:?}", args);
+
+	let path = &args[1];
+
+	Command::new("cd")
+		.current_dir(path)
         .spawn()
-        .expect("ls command failed to start");
+        .expect("command failed to start");
 }
