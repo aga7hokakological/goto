1f29ef94c0b17670c095d013a424f3f31b61ed1e
aga7hokakological <iamsaurabhmandavkar@gmail.com>
2020-09-05 22:17:19 +0530

    dir found

--- src/main.rs
+++ src/main.rs
@@ -6,52 +6,14 @@ use std::process::{Command, Stdio};
 
 use walkdir::WalkDir;
 
+extern crate regex;
+use regex::Regex;
+
 extern crate skim;
 use skim::prelude::*;
 
 // pub mod terminal;
 
-// fn current_dir() -> io::Result<()>{
-// 	let current_dir = match env::current_dir() {
-//     // println!("{:?}", current_dir);
-
-//     	// Ok(current_dir) => println!("{:?}", current_dir),
-//     	Ok(current_dir) => current_dir,
-//     	Err(e) => return Err(e),
-// 	};
-
-// 	println!("{:?}", current_dir);
-// 	// return Ok(current_dir)
-// 	Ok(())
-// }
-
-// #[shell]
-// fn change_dir(home_dir: PathBuf, current_dir: PathBuf) {
-// 	println!("{:?}", home_dir);
-// }
-
-// fn search(path: String) -> Result<(), io::Error> {
-// 	for entry in WalkDir::new(&path) {
-//     	println!("{}", entry.path().display());
-// 	}
-// }
-
-use path_clean::PathClean;
-
-fn absolute_path<P>(path: P) -> io::Result<PathBuf>
-where
-    P: AsRef<Path>,
-{
-    let path = path.as_ref();
-    let absolute_path = if path.is_absolute() {
-        path.to_path_buf()
-    } else {
-        env::current_dir()?.join(path)
-    }.clean();
-
-    println!("{:?}", absolute_path);
-    Ok(absolute_path)
-}
 
 // fn create_path(home_dir: &std::path::PathBuf, arg_path: &std::string::String) -> io::Result<()> {
 // 	for entry in fs::read_dir(home_dir)? {
@@ -61,9 +23,12 @@ where
 // 	Ok(())
 // }
 
-fn recursive_file_traversal<P>(home_dir: P) 
+fn recursive_file_traversal<P>(home_dir: P, arg_path: &std::string::String)
 where
-    P: AsRef<Path>{
+    P: AsRef<Path> {
+
+    let re = Regex::new(r"(\w+)$").unwrap();
+
     for entry in fs::read_dir(home_dir).unwrap(){
         let path_buffer =  entry.unwrap().path();
         let file_path= path_buffer.display().to_string();        
@@ -74,18 +39,30 @@ where
             {
                 let attributes =  attrib_result.unwrap();
 
-                if attributes.is_dir()
-                {
+                if attributes.is_dir() {
+
                     let new_directory = file_path.clone();
-                    recursive_file_traversal(new_directory);
+                    // println!("{:?}", new_directory);
+                    let match_path = re.find(&new_directory).unwrap();
+                    // println!("{:?}", match_path);
+
+                    let start = match_path.start();
+                    let end = match_path.end();
+
+                    if &new_directory[start..end] == arg_path.as_str() {
+                    	println!("{:?}", new_directory);
+                    } else {
+                    	recursive_file_traversal(new_directory, arg_path);
+                    }
                 }
-                else if attributes.is_file()
-                {
-                    println!("{}", file_path.clone());
-                }
+                // else if attributes.is_file()
+                // {
+                //     println!("{}", file_path.clone());
+                // }
             }
         }
     }
+    // return new_directory
 }
 
 fn main() {
@@ -105,28 +82,31 @@ fn main() {
 	// let goto_dir = PathBuf::from(&path);
 	// println!("{:?}", fs::canonicalize(&goto_dir));
 
-	let mut p = PathBuf::from("");
-	p.push(&path::MAIN_SEPARATOR.to_string());
-	p.push(&arg_path);
-	println!("{}", p.to_str().unwrap());
+	// let mut p = PathBuf::from("");
+	// p.push(&path::MAIN_SEPARATOR.to_string());
+	// p.push(&arg_path);
+	// println!("{}", p.to_str().unwrap());
 
 	// println!("{:?}", canonicalize(&p));
 
-	absolute_path(&p);
+	// absolute_path(&p);
 
-	recursive_file_traversal(&home_dir);
+	recursive_file_traversal(&home_dir, arg_path);
+
+	// println!("{:?}", path);
 
 	// create_path(&home_dir, arg_path);
 
-	let path1 = Path::new(&p);
-	env::set_current_dir(&path1).is_ok();
-	println!("Successfully changed working directory to {}!", path1.display());
+	// let path1 = Path::new(&arg_path);
+	// env::set_current_dir(&path1).is_ok();
+	// println!("Successfully changed working directory to {}!", path1.display());
 
 	// change_dir(&home_dir, &current_dir);
-	Command::new("ls")
-		.current_dir(&path1)
-        .spawn()
-        .expect("command failed to start");
+
+	// Command::new("ls")
+	// 	.current_dir(&path1)
+ //        .spawn()
+ //        .expect("command failed to start");
 
  	// let path = current_dir();
  	// println!("{:?}", path);
